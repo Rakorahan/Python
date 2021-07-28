545838c0a2d49f071c56b35bcb3f226b9c8b094f
Rakorahan <84695281+Rakorahan@users.noreply.github.com>
2021-07-28 14:45:01 +0800

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,57 @@
+import random
+
+
+print("This is a dice game")
+
+
+
+
+while True:
+
+    number = random.randint(1,6)
+
+    if number == 1:
+        print("----------")
+        print("|        |")
+        print("|    O   |")
+        print("|        |")
+        print("----------")
+    if number == 2:
+        print("----------")
+        print("|        |")
+        print("| O    O |")
+        print("|        |")
+        print("----------")
+    if number == 3:
+        print("----------")
+        print("|    O   |")
+        print("|    O   |")
+        print("|    O   |")
+        print("----------")
+    if number == 4:
+        print("----------")
+        print("| O    O |")
+        print("|        |")
+        print("| O    O |")
+        print("----------")
+    if number == 5:
+        print("----------")
+        print("| O    O |")
+        print("|    O   |")
+        print("| O    O |")
+        print("----------")
+    if number == 6:
+        print("----------")
+        print("| O    O |")
+        print("| O    O |")
+        print("| O    O |")
+        print("----------")
+
+
+    key = input("Enter X for Again: ")
+    
+    if key in "x":
+        continue
+    else:
+        print("Wrong Keyword")
+        break
